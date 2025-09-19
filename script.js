cbe931e704b8609d2fd5f26ea299e2365bfaf08f
taplamdevxin <teamanhlam@gmail.com>
2025-09-20 04:09:25 +0700

    modify duration

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('.effect-container');
-    const texts = ["Yêu vợ yêu nhiều nhiều nhiều", "My love", "Forever", "Trên trời có đám mây xanh", "Dưới đất có anh yêu em rất nhiều 🤍"];
+    const texts = ["Yêu vợ yêu nhiều nhiều nhiều"];
     const characters = ["❤️", "💖", "💘", "✨"];
 
     function createEffectElement() {
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const size = Math.random() * 20 + 10; // Random size
         const left = Math.random() * 100; // Random horizontal position
-        const duration = Math.random() * 5 + 7; // Random animation duration
+        const duration = Math.random() * 5 + 5; // Random animation duration
 
         element.style.left = `${left}vw`;
         element.style.fontSize = `${size}px`;
@@ -28,9 +28,9 @@ document.addEventListener('DOMContentLoaded', () => {
         // Remove the element after the animation is finished to save memory
         setTimeout(() => {
             element.remove();
-        }, duration * 1000);
+        }, duration * 800);
     }
 
     // Create a new element every 300 milliseconds
-    setInterval(createEffectElement, 300);
+    setInterval(createEffectElement, 400);
 });
